916c7371be9eaf278c76e9cb430bb14c63706692
Robin Castillo <30296610+castillorobin@users.noreply.github.com>
2023-03-09 10:26:42 -0600

    Primer commit

---
+++ calcularsalario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcularsalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = 0, coshora = 0, salario = 0;
+            Console.WriteLine("Calcular el salario a pagar");
+            Console.WriteLine("Ingrese la cantidad de horas");
+            horas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la hora");
+            coshora = Convert.ToInt32(Console.ReadLine());
+            salario = horas * coshora;
+            Console.WriteLine("El total a pagar es: " + salario);
+            Console.ReadLine();
+        }
+    }
+}
